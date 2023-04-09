b9610a0fe788300adf67d61fa0fb9712b2989be7
Mrowr Purr <87039589+mrowrpurr@users.noreply.github.com>
2023-04-09 13:39:17 -0700

    Update xmake.lua

--- packages/s/StringFormatting/xmake.lua
+++ packages/s/StringFormatting/xmake.lua
@@ -1,5 +1,5 @@
 package("StringFormatting")
-    set_kind("library") -- Change this to library
+    set_kind("library")
     set_homepage("https://github.com/MrowrLib/StringFormatting.h")
     set_description("A header-only library for string formatting.")
     add_urls("https://github.com/MrowrLib/StringFormatting.h.git")
