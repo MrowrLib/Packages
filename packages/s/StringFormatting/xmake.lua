6e2336d4b022ec9971cddfc300a76102056dc60e
Mrowr Purr <mrowr.purr@gmail.com>
2023-04-09 13:07:11 -0700

    s/headeronly/library and add on_test

--- packages/s/StringFormatting/xmake.lua
+++ packages/s/StringFormatting/xmake.lua
@@ -1,8 +1,20 @@
 package("StringFormatting")
-    set_kind("headeronly")
+    set_kind("library") -- Change this to library
     set_homepage("https://github.com/MrowrLib/StringFormatting.h")
     set_description("A header-only library for string formatting.")
     add_urls("https://github.com/MrowrLib/StringFormatting.h.git")
     on_install(function (package)
         os.cp("include", package:installdir())
+    end)
+    on_test(function (package) -- Add this on_test function
+        local configs = {}
+        if package:config("use_fmt") then
+            configs.use_fmt = true
+        end
+        assert(package:check_cxxsnippets({configs = configs, test = [=[#include <StringFormatting/StringFormatting.h>
+#include <iostream>
+int main() {
+    std::cout << string_format("Hello {}!", "World") << std::endl;
+}
+]=]}))
     end)
