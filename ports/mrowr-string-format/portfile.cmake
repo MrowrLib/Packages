38dc1846d06fb1a20abac36b285e4227f04aa864
Mrowr Purr <mrowr.purr@gmail.com>
2023-04-15 19:26:05 -0700

    Add new port mrowr-string-format

--- ports/mrowr-string-format/portfile.cmake
+++ ports/mrowr-string-format/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO https://github.com/MrowrLib/string_format.cpp.git
-    REF f50ff588425b562e5965a7170ca1dbb19b2a93ce
+    URL https://github.com/MrowrLib/string_format.cpp.git
+    REF 302770276d1b981f3b434b2639ddd07ce6642917
 )
 
 vcpkg_cmake_configure(
