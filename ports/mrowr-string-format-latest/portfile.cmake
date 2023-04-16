fe12c1a41f4c8f24717f7a51a72b0fd3b39336b6
Mrowr Purr <mrowr.purr@gmail.com>
2023-04-15 18:17:04 -0700

    Add new port mrowr-string-format-latest

---
+++ ports/mrowr-string-format-latest/portfile.cmake
@@ -0,0 +1,16 @@
+file(DOWNLOAD "https://api.github.com/repos/MrowrLib/MrowrLib/tarball/main" ${DOWNLOADS}/archive.tar.gz
+    SHOW_PROGRESS
+)
+
+vcpkg_extract_source_archive(
+    SOURCE_PATH
+    ARCHIVE ${DOWNLOADS}/archive.tar.gz
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/string_format)
