2a35d5dd71f595c5dfce55cd77e28ce46941351c
mrowrpurr <87039589+mrowrpurr@users.noreply.github.com>
2023-04-20 09:33:48 -0700

    Update mrowr-log 2023-04-20-6e5665e (2023-04-20 09:33:48)

--- ports/mrowr-log/portfile.cmake
+++ ports/mrowr-log/portfile.cmake
@@ -13,5 +13,5 @@ vcpkg_cmake_install()
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/_Log_)
+vcpkg_cmake_config_fixup(CONFIG_PATH share/_Log_)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
