43d7d748af7fff55f55768ed049a49d84eca1370
mrowrpurr <87039589+mrowrpurr@users.noreply.github.com>
2023-04-21 00:06:32 -0700

    Update mrowr-log 2023-04-20-16841a3 (2023-04-21 00:06:32)

--- ports/mrowr-log/portfile.cmake
+++ ports/mrowr-log/portfile.cmake
@@ -4,13 +4,13 @@ vcpkg_from_git(
     REF 16841a3d6c005959af7de7366c7469200280d608
 )
 
-set(PACKAGE_NAME _Log_)
+set(LIBRARY_NAME _Log_)
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 
 vcpkg_cmake_install()
 
-file(INSTALL "${CURRENT_PACKAGES_DIR}/share/${PORT}/" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PACKAGE_NAME}")
+file(INSTALL "${CURRENT_PACKAGES_DIR}/share/${PORT}/" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${LIBRARY_NAME}")
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/share/${PORT}/*/**"
