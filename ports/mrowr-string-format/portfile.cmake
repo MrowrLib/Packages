85e912e5952d9bcfd92d7bcbaec8aa27e56b07d5
mrowrpurr <87039589+mrowrpurr@users.noreply.github.com>
2023-04-21 07:49:03 -0700

    Update mrowr-string-format 2023-04-20-45e8db5 (2023-04-21 07:49:03)

--- ports/mrowr-string-format/portfile.cmake
+++ ports/mrowr-string-format/portfile.cmake
@@ -4,22 +4,16 @@ vcpkg_from_git(
     REF 45e8db50ba8a68b6fda60a31b41c37411aa156ad
 )
 
-set(LIBRARY_NAME string_format)
-
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS -DBUILD_EXAMPLE=OFF
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
 vcpkg_cmake_install()
 
-file(COPY ${CURRENT_PACKAGES_DIR}/share/${PORT}/ DESTINATION ${CURRENT_PACKAGES_DIR}/share/${LIBRARY_NAME})
-
 file(REMOVE_RECURSE
-    "${CURRENT_PACKAGES_DIR}/share/${PORT}/*/**"
     "${CURRENT_PACKAGES_DIR}/debug"
     "${CURRENT_PACKAGES_DIR}/lib"
+    "${CURRENT_PACKAGES_DIR}/share/${PORT}"
 )
 
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(COPY "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
