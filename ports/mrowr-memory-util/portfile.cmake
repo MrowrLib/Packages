31c39e50fe46d8c0855c76067015e46c9493e6b5
mrowrpurr <87039589+mrowrpurr@users.noreply.github.com>
2023-04-21 08:08:05 -0700

    Add new port mrowr-memory-util

---
+++ ports/mrowr-memory-util/portfile.cmake
@@ -0,0 +1,19 @@
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL https://github.com/MrowrLib/memory_util.git
+    REF 9eb9d57b1d9ba8c0c2ee55472d20d06474d63b93
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+)
+
+vcpkg_cmake_install()
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug"
+    "${CURRENT_PACKAGES_DIR}/lib"
+)
+
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
