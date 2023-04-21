43c2efe75e37137dfb6a5e1dd1b5ec04d1d5bdc2
mrowrpurr <87039589+mrowrpurr@users.noreply.github.com>
2023-04-20 23:40:12 -0700

    Update mrowr-string-format 2023-04-20-45e8db5 (2023-04-20 23:40:12)

--- ports/mrowr-string-format/portfile.cmake
+++ ports/mrowr-string-format/portfile.cmake
@@ -4,6 +4,10 @@ vcpkg_from_git(
     REF 45e8db50ba8a68b6fda60a31b41c37411aa156ad
 )
 
+message("What is PORT? ${PORT}")
+message("What is SOURCE PATH? ${SOURCE_PATH}")
+message("What is CURRENT_PACKAGES_DIR? ${CURRENT_PACKAGES_DIR}")
+
 configure_file("${SOURCE_PATH}/LICENSE" "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright" COPYONLY)
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
