ecec19f7c25732f0b7c74a96fa41fdc4f8a8821b
mrowrpurr <87039589+mrowrpurr@users.noreply.github.com>
2023-04-21 07:26:57 -0700

    Update mrowr-log 2023-04-20-16841a3 (2023-04-21 07:26:57)

--- ports/mrowr-log/portfile.cmake
+++ ports/mrowr-log/portfile.cmake
@@ -9,17 +9,16 @@ message(STATUS "PORT: ${PORT}")
 message(STATUS "SOURCE_PATH: ${SOURCE_PATH}")
 message(STATUS "CURRENT_PACKAGES_DIR: ${CURRENT_PACKAGES_DIR}")
 
-# Print out whether CURRENT_PACKAGES_DIRECTORY/share/${PORT} exists
 if(EXISTS "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-    message(STATUS "CURRENT_PACKAGES_DIR/share/${PORT} exists")
+    message(STATUS "${CURRENT_PACKAGES_DIR}/share/${PORT} exists")
 else()
-    message(STATUS "CURRENT_PACKAGES_DIR/share/${PORT} does not exist")
+    message(STATUS "${CURRENT_PACKAGES_DIR}/share/${PORT} does not exist")
 endif()
 
 message("Configuring...")
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS -DBUILD_EXAMPLE=OFF
 )
 
@@ -33,22 +32,21 @@ message(STATUS "PORT: ${PORT}")
 message(STATUS "SOURCE_PATH: ${SOURCE_PATH}")
 message(STATUS "CURRENT_PACKAGES_DIR: ${CURRENT_PACKAGES_DIR}")
 
-# Print out whether CURRENT_PACKAGES_DIRECTORY/share/${PORT} exists
 if(EXISTS "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-    message(STATUS "CURRENT_PACKAGES_DIR/share/${PORT} exists")
+    message(STATUS "${CURRENT_PACKAGES_DIR}/share/${PORT} exists")
 else()
-    message(STATUS "CURRENT_PACKAGES_DIR/share/${PORT} does not exist")
+    message(STATUS "${CURRENT_PACKAGES_DIR}/share/${PORT} does not exist")
 endif()
 
-file(COPY "${CURRENT_PACKAGES_DIR}/share/${PORT}/" DESTINATION "${CURRENT_PACKAGES_DIR}/share/_Log_")
+# file(COPY "${CURRENT_PACKAGES_DIR}/share/${PORT}/" DESTINATION "${CURRENT_PACKAGES_DIR}/share/_Log_")
 
 # file(COPY "${CURRENT_PACKAGES_DIR}/share/${PORT}/" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${LIBRARY_NAME}")
 
 # "${CURRENT_PACKAGES_DIR}/share/${PORT}/*/**"
-file(REMOVE_RECURSE
-    "${CURRENT_PACKAGES_DIR}/debug"
-    "${CURRENT_PACKAGES_DIR}/lib"
-)
+# file(REMOVE_RECURSE
+# "${CURRENT_PACKAGES_DIR}/debug"
+# "${CURRENT_PACKAGES_DIR}/lib"
+# )
 
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(COPY "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+# file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+# file(COPY "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
