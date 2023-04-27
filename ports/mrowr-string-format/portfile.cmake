eb6626b4398a5813eef7195e3a05ea203125a78a
Mrowr Purr <mrowr.purr@gmail.com>
2023-04-26 17:51:50 -0700

    Update mrowr-string-format to 2023-04-27-f5424a4

--- ports/mrowr-string-format/portfile.cmake
+++ ports/mrowr-string-format/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/MrowrLib/string_format.h.git
-    REF 9e07604bd677e4784abbd6b38128bbf216ad2ca5
+    REF f5424a484e2daf12ff7617ba50c59a90998240eb
 )
 
 vcpkg_cmake_configure(
