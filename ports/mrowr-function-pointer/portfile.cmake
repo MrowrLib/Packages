895e2bbf676e1c89b96fb258c7b734cffb9b095d
Mrowr Purr <mrowr.purr@gmail.com>
2023-07-02 08:44:29 -0700

    Update mrowr-function-pointer to 2023-07-02-887bf72

--- ports/mrowr-function-pointer/portfile.cmake
+++ ports/mrowr-function-pointer/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/MrowrLib/function_pointer.h.git
-    REF bd77ee48a87bf5e9747f208491a7ae67a2f24c04
+    REF 887bf727b61a5b096a17c2101f60cb4e97dbbbeb
 )
 
 vcpkg_cmake_configure(
