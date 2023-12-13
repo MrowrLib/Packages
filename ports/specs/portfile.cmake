6d7fb478de7f547e3799010c236216eb2255d6f8
Mrowr Purr <mrowr.purr@gmail.com>
2023-12-13 14:03:15 -0800

    Update specs to 2023-12-13-efa9d3e

--- ports/specs/portfile.cmake
+++ ports/specs/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/mrowrpurr/Specs.cpp.git
-    REF b8f48c72a1c443b5a7a4605f416201f38473f5cf
+    REF efa9d3ef1536e01b9424e3dcf1ce9b0c911cccd3
 )
 
 vcpkg_cmake_configure(
