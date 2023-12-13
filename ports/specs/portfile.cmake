82ef2c57416ca6c13bedc24fd67aa5ba8851e7d0
Mrowr Purr <mrowr.purr@gmail.com>
2023-12-13 15:13:15 -0800

    Don't remove debug folder

--- ports/specs/portfile.cmake
+++ ports/specs/portfile.cmake
@@ -14,9 +14,5 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-file(REMOVE_RECURSE
-    "${CURRENT_PACKAGES_DIR}/debug"
-)
-
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
