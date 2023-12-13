cb6b474213e2e17591f418cf70402647270ac770
Mrowr Purr <mrowr.purr@gmail.com>
2023-12-13 11:35:33 -0800

    Add options for Specs.cpp

--- ports/specs/portfile.cmake
+++ ports/specs/portfile.cmake
@@ -6,6 +6,10 @@ vcpkg_from_git(
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DBUILD_SPECS=ON
+        -DBUILD_SNOWHOUSE_ADAPTER=OFF
+        -DBUILD_LIBASSERT_ADAPTER=OFF
 )
 
 vcpkg_cmake_install()
