7f46769f9522bba45061df717dc270e3989a5710
Mrowr Purr <mrowr.purr@gmail.com>
2023-12-13 15:17:34 -0800

    Remove debug/include (per warning)

--- ports/specs/portfile.cmake
+++ ports/specs/portfile.cmake
@@ -14,5 +14,9 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+)
+
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
