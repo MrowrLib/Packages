b4a616b7e5b1011afbedc2afebb627d0189b56c1
Mrowr Purr <mrowr.purr@gmail.com>
2023-12-13 16:26:54 -0800

    Remove release *.lib when in debug mode

--- ports/specs/portfile.cmake
+++ ports/specs/portfile.cmake
@@ -15,8 +15,11 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 if(VCPKG_BUILD_TYPE STREQUAL "DEBUG")
+    # Delete all the release lib files
+    file(GLOB LIBS "${CURRENT_PACKAGES_DIR}/lib/*.lib")
+
     # Copy everything from debug/lib into lib
-    file(GLOB_RECURSE LIBS "${CURRENT_PACKAGES_DIR}/debug/lib/*.lib")
+    file(GLOB LIBS "${CURRENT_PACKAGES_DIR}/debug/lib/*.lib")
     file(COPY ${LIBS} DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
 endif()
 
