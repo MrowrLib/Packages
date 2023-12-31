03d80252b5509771f9b2a0da8c92c4705b687681
Mrowr Purr <mrowr.purr@gmail.com>
2023-12-31 15:46:58 -0800

    Update mrowr-void-pointer to 2023-12-31-7fe9da5

--- ports/mrowr-void-pointer/portfile.cmake
+++ ports/mrowr-void-pointer/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/MrowrLib/void_pointer.h.git
-    REF e171a2b7c3c37b2a0ee79fea8deea558bdea47ac
+    REF 7fe9da5aacd33bc3c26aad5c063f656a39981c45
 )
 
 vcpkg_cmake_configure(
